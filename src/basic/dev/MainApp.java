05322913f5222155987f38c4d79d8936c913a847
ngomanhthong <113655638+ngomanhthong@users.noreply.github.com>
2022-10-06 08:20:42 +0700

    Add files via upload

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,33 @@
+package basic.dev;
+/**
+ * Ghichú(comment nhiều dòng)
+ * @author ngomanhthong
+ *1. Tênlớp trùng tênfile
+ *2. Tênlớp chữcái đầu viết hoa. Nếu từghép thìnhững chữcái dầu viết hoa
+ *4. Tênlớp đặt phải cóýnghĩa. Vídụ: class Peson: lớp môtảconngười
+ * -Interface: Phải cótiền tốI. Vídụ: IListener
+ * -Abstract: Phải cótiền tốAbstract. Vídụ: AbstractShape
+ */
+public class MainApp {
+	//Comment 1 dòng cách 1. Phím nóng Ctr+ /
+	/*Comment 1 dòng cách 2*/
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		/**
+		 * Cách đặt tênbiến:
+		 * 1. Khôngchứa kýtựđặc biệt !@#$%^&*()-+=
+		 * 2. Khôngchứa khoảng trắng
+		 * 3. Viết thường
+		 * 4. Khôngđược bắt đầu bằng số
+		 */
+		int number;
+		String nameSchool;
+		
+	}
+	/*
+	 *  tênphươngthức tuântheoquytắc camelCase
+	 */
+	public void show() {
+		
+	}
+}
